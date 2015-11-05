22cc7e2f6d58eeab563b130243e88425cd325766
Ainsey11 <robert@ainsey11.com>
2015-11-05 21:26:11 +0000

    Started WSUS Pending Updates

--- Resources/Get-WSUSPendingUpdates.ps1
+++ Resources/Get-WSUSPendingUpdates.ps1
@@ -1,1 +1,9 @@
-ï»¿
+#requires -Version 3
+
+# Pull in vars
+$vars = (Get-Item $PSScriptRoot).Parent.FullName + '\vars.ps1'
+Invoke-Expression -Command ($vars)
+
+[void][reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
+$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer(“$wsuserver”,$False)
+$wsus.GetUpdates()
