20a6bb8aff29eea6c47328f766582facc45b6eea
Ainsey11 <robert@ainsey11.com>
2015-11-06 00:05:45 +0000

    moar

--- Resources/Get-WSUSPendingUpdates.ps1
+++ Resources/Get-WSUSPendingUpdates.ps1
@@ -4,34 +4,25 @@
 #$vars = (Get-Item $PSScriptRoot).Parent.FullName + '\vars.ps1'
 #Invoke-Expression -Command ($vars)
 
-[void][reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
-$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer(“thq-wsus01”,$False)
-$Updates = $wsus.GetUpdates()
-$NumberofUpdates = $updates.count
+#[void][reflection.assembly]::LoadWithPartialName("Microsoft.UpdateServices.Administration")
+#$wsus = [Microsoft.UpdateServices.Administration.AdminProxy]::getUpdateServer(“10.159.25.24”,$False,8530)
+#$Unapproved = $wsus.GetUpdates() | Where {$_.IsApproved -eq $False -and $_.IsDeclined -eq $False}
+$NumberUnapproved = 8 #$Unapproved.Count
 
-#InfluxDBFuntime!
-$points.Add('WSUSPendingUpdates',$NumberofUpdates)
-
-# Wrap the points into a null array to meet InfluxDB json requirements. Sad face
-[System.Collections.ArrayList]$nullarray = @()
-$nullarray.Add($points)
+# Stick the data points into the null array for required JSON format
+[System.Collections.ArrayList]$nullpoints = @()
+$nullpoints.Add($NumberUnapproved)
 
 # Build the post body
 $body = @{}
-$body.Add('WSUSPendingUpdates')
-$body.Add('columns',@('Count'))
-$body.Add('points',$nullarray)
+$body.Add('name',"unnaprovedupdates")
+$body.Add('columns',"number")
+$body.Add('points',$nullpoints)
 
 # Convert to json
 $finalbody = $body | ConvertTo-Json
 
 # Post to API
-try 
-{
-    $r = Invoke-WebRequest -Uri $global:url -Body ('['+$finalbody+']') -ContentType 'application/json' -Method Post -ErrorAction:Stop
-    Write-Host -Object "Data has been posted, status is $($r.StatusCode) $($r.StatusDescription)"        
-}
-catch 
-{
-    throw 'Could not POST to InfluxDB API endpoint'
-}
+
+ Invoke-WebRequest -Uri "http://10.159.25.13:8086/db/DB1/series?u=dash&p=dash" -Body ('['+$finalbody+']') -ContentType 'application/json' -Method Post -ErrorAction:Stop
+ 
