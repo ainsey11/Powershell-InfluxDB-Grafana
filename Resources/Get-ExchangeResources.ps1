c5dd76a81441b88bf99c732a5417c3a1df460ac9
Ainsey11 <robert@ainsey11.com>
2015-11-06 20:08:55 +0000

    started adding perf counters

--- Resources/Get-ExchangeResources.ps1
+++ Resources/Get-ExchangeResources.ps1
@@ -1,2 +1,10 @@
-﻿Get-Counter '\Memory\Available MBytes'
-Get-Counter '\Processor(_Total)\% Processor Time'
+﻿# Simple one, gets CPU and RAM usage of the mail servers
+
+$thqmail01cpu = Get-Counter -ComputerName "thq-mail01" '\Processor(_Total)\% Processor Time'
+$thqmail02cpu = Get-Counter -ComputerName "thq-mail02" '\Processor(_Total)\% Processor Time'
+$batmail01cpu = Get-Counter -ComputerName "bat-mail01" '\Processor(_Total)\% Processor Time'
+$thqmail01ram = Get-Counter -ComputerName "thq-mail01" '\Processor(_Total)\% Processor Time'
+$thqmail02ram = Get-Counter -ComputerName "thq-mail02" '\Processor(_Total)\% Processor Time'
+$batmail01ram = Get-Counter -ComputerName "bat-mail01" '\Processor(_Total)\% Processor Time'
+
+(get-Counter -ListSet memory).paths
