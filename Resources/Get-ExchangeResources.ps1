cd1237d3fb548b0d843930042b8c0e511cd53121
Ainsey11 <robert@ainsey11.com>
2015-11-10 10:09:57 +0000

    tweaks

--- Resources/Get-ExchangeResources.ps1
+++ Resources/Get-ExchangeResources.ps1
@@ -1,31 +1,20 @@
 ﻿Function Get-ExchangeResources{ # Lets import our list of computers
 param(
-    [string]$computer
+    [string]$computer = "localhost"
     )
-
-    # Lets get our stats
-    # Lets create a re-usable WMI method for CPU stats
-    $ProcessorStats = Get-WmiObject win32_processor -computer $computer
-    $ComputerCpu = $ProcessorStats.LoadPercentage 
-    # Lets create a re-usable WMI method for memory stats
-    $OperatingSystem = Get-WmiObject win32_OperatingSystem -computer $computer
-    # Lets grab the free memory
-    $FreeMemory = $OperatingSystem.FreePhysicalMemory
-    # Lets grab the total memory
-    $TotalMemory = $OperatingSystem.TotalVisibleMemorySize
-    # Lets do some math for percent
-    $MemoryUsed = ($FreeMemory/ $TotalMemory) * 100
-    $PercentMemoryUsed = "{0:N2}" -f $MemoryUsed
+    $freemem = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computer
+    $FreePhysicalMem = ([math]::round($freemem.FreePhysicalMemory / 1024, 2))
+    $cpuusage = Get-Counter '\Processor(_Total)\% Processor Time'
     
-    Write-Host -ForegroundColor Green "$computername"
-    Write-Host -ForegroundColor Green "RAM USed :$PercentMemoryUsed"
-    Write-Host -ForegroundColor Green "CPU Used :$ComputerCpu"
+    Write-Host -ForegroundColor Green "$computer"
+    Write-Host -ForegroundColor Green "RAM USed :$FreePhysicalMem"
+    Write-Host -ForegroundColor Green "CPU Used :$cpuusage"
 
 # API funkiness now:
 
 [System.Collections.ArrayList]$ExchangeResources = @()
-$ExchangeStats.Add($PercentMemoryUsed)
-$ExchangeStats.Add($ComputerCPU)
+$ExchangeStats.Add($FreePhysicalMem)
+$ExchangeStats.Add($cpuusage)
  
 # Stick the data points into the null array for required JSON format
 [System.Collections.ArrayList]$nullpoints = @()
@@ -33,7 +22,7 @@ $nullpoints.Add($Exchangeresouces)
  
 # Build the post body
 $body = @{}
-$body.Add('name',"$computername")
+$body.Add('name',"$computer")
 $body.Add('columns',@('MemoryUsed', 'CPU'))
 $body.Add('points',$nullpoints)
  
@@ -44,6 +33,6 @@ $finalbody = $body | ConvertTo-Json  -Compress
 Invoke-WebRequest -Uri "http://10.159.25.13:8086/db/DB1/series?u=dash&p=dash" -Body ('['+$finalbody+']') -ContentType 'application/json' -Method Post -ErrorAction:Continue
 }
 
-Get-ExchangeResources -computer "thq-mail01"
-Get-ExchangeResources -computer "thq-mail02"
-Get-ExchangeResources -computer "bat-mail01"
+Get-ExchangeResources -computer "localhost"
+#Get-ExchangeResources -computer "thq-mail02"
+#Get-ExchangeResources -computer "bat-mail01"
