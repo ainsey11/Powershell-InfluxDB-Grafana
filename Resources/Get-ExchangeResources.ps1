b8bc3405b03bbc1b5631f59af25e214ea605eb0a
Ainsey11 <robert@ainsey11.com>
2015-11-10 13:49:24 +0000

    Now all working and sending data to API

--- Resources/Get-ExchangeResources.ps1
+++ Resources/Get-ExchangeResources.ps1
@@ -1,38 +1,44 @@
 ﻿Function Get-ExchangeResources{ # Lets import our list of computers
 param(
-    [string]$computer = "localhost"
+    [string]$computer
     )
-    $freemem = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computer
-    $FreePhysicalMem = ([math]::round($freemem.FreePhysicalMemory / 1024, 2))
-    $cpuusage = Get-Counter '\Processor(_Total)\% Processor Time'
+  $OperatingSystem = Get-WmiObject win32_OperatingSystem -computer $computer
+  $FreeMemory = $OperatingSystem.FreePhysicalMemory
+  $TotalMemory = $OperatingSystem.TotalVisibleMemorySize
+  $MemoryUsed = $MemoryUsed = 100 - (($FreeMemory/ $TotalMemory) * 100)
+  $PercentMemoryUsed = "{0:N2}" -f $MemoryUsed
+       
+    $cpu = Get-WmiObject win32_processor
+    $cpuusage = $cpu.LoadPercentage
+    
+    $seriesname = $computer+"resources"
     
     Write-Host -ForegroundColor Green "$computer"
-    Write-Host -ForegroundColor Green "RAM USed :$FreePhysicalMem"
+    Write-Host -ForegroundColor Green "RAM Used :$PercentMemoryUsed"
     Write-Host -ForegroundColor Green "CPU Used :$cpuusage"
 
-# API funkiness now:
-
-[System.Collections.ArrayList]$ExchangeResources = @()
-$ExchangeStats.Add($FreePhysicalMem)
-$ExchangeStats.Add($cpuusage)
+    [System.Collections.ArrayList]$Exchangeresources = @()
+    $Exchangeresources.Add($PercentMemoryUsed)
+    $Exchangeresources.Add($cpuusage)
  
-# Stick the data points into the null array for required JSON format
-[System.Collections.ArrayList]$nullpoints = @()
-$nullpoints.Add($Exchangeresouces)
+    # Stick the data points into the null array for required JSON format
+    [System.Collections.ArrayList]$nullpoints = @()
+    $nullpoints.Add($Exchangeresources)
  
-# Build the post body
-$body = @{}
-$body.Add('name',"$computer")
-$body.Add('columns',@('MemoryUsed', 'CPU'))
-$body.Add('points',$nullpoints)
+    # Build the post body
+    $body = @{}
+    $body.Add('name',"$seriesname")
+    $body.Add('columns',@('Ram', 'CPU'))
+    $body.Add('points',$nullpoints)
  
-# Convert to json
-$finalbody = $body | ConvertTo-Json  -Compress
-
-# Post to API
-Invoke-WebRequest -Uri "http://10.159.25.13:8086/db/DB1/series?u=dash&p=dash" -Body ('['+$finalbody+']') -ContentType 'application/json' -Method Post -ErrorAction:Continue
+    # Convert to json
+    $finalbody = $body | ConvertTo-Json  -Compress
+    $finalbody
+  # Post to API
+ 
+ Invoke-WebRequest -Uri "http://10.159.25.13:8086/db/DB1/series?u=dash&p=dash" -Body ('['+$finalbody+']') -ContentType 'application/json' -Method Post -ErrorAction:Continue
 }
 
-Get-ExchangeResources -computer "localhost"
-#Get-ExchangeResources -computer "thq-mail02"
-#Get-ExchangeResources -computer "bat-mail01"
+Get-ExchangeResources -computer "thq-mail01"
+Get-ExchangeResources -computer "thq-mail02"
+Get-ExchangeResources -computer "bat-mail01"
