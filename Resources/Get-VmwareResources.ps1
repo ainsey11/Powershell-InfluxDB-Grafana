08cdb71aed80ef2ec754cfdf2a052d8d44712fa5
Ainsey11 <robert@ainsey11.com>
2015-12-03 15:15:06 +0000

    Tweaked

--- Resources/Get-VmwareResources.ps1
+++ Resources/Get-VmwareResources.ps1
@@ -3,10 +3,12 @@ Connect-VIServer "thq-vcc01"
 New-VIProperty -Name PercentFree -ObjectType Datastore -Value {"{0:N2}" -f ($args[0].FreeSpaceMB/$args[0].CapacityMB*100)} -Force
 
 function Get-VDIStats{
+
+
 $NumberofSMBVDI = (Get-VM -Name "*smb*"| where {$_.Powerstate -eq "PoweredOn"}).count
 $NumberofNOCVDI = (Get-VM -Name "*noc*"| where {$_.Powerstate -eq "PoweredOn"}).count
 $DatastoreFreeSpace = Get-Datastore -Name "thq-vdi01-01-datastore01" | foreach { $_.PercentFree}
- 
+
 # API funkiness now:
 
 #Building the array
@@ -14,7 +16,6 @@ $DatastoreFreeSpace = Get-Datastore -Name "thq-vdi01-01-datastore01" | foreach {
 $VdiStats.Add($NumberofSMBVDI)
 $VdiStats.Add($NumberofNOCVDI)
 $VdiStats.Add($DatastoreFreeSpace)
-
 
 # Stick the data points into the null array for required JSON format
 [System.Collections.ArrayList]$nullpoints = @()
