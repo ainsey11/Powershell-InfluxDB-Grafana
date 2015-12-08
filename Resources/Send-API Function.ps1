a5e62ea97e2409718ecc46f81b1cfa4769b5aa42
Ainsey11 <robert@ainsey11.com>
2015-12-08 15:09:40 +0000

    handy for testing

---
+++ Resources/Send-API Function.ps1
@@ -0,0 +1,27 @@
+﻿function Send-API {
+
+param ([string] $seriesname),
+      ([string] $column)
+      ([string] $point)
+
+ 
+#Building the array
+[System.Collections.ArrayList]$array = @()
+$array.Add($point)
+
+# Stick the data points into the null array for required JSON format
+[System.Collections.ArrayList]$nullpoints = @()
+$nullpoints.Add($array)
+ 
+# Build the post body
+$body = @{}
+$body.Add('name',$seriesname)
+$body.Add('columns',@($column))
+$body.Add('points',$nullpoints)
+ 
+# Convert to json
+$finalbody = $body | ConvertTo-Json  -Compress
+ 
+# Post to API
+Invoke-WebRequest -Uri "http://thq-dash01:8086/db/DB1/series?u=dash&p=dash" -Body ('['+$finalbody+']') -ContentType 'application/json' -Method Post -ErrorAction:Continue
+}
